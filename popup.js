588ae71d88adf2661bbd23dcfdafba0c6a1fe17d
firemax13 <83276292+firemax13@users.noreply.github.com>
2021-04-29 14:17:45 +0800

    Create popup.js

---
+++ popup.js
@@ -0,0 +1,7 @@
+<script>
+// When the user clicks on <div>, open the popup
+function myFunction() {
+  var popup = document.getElementById("mybtcpopup");
+  popup.classList.toggle("show");
+}
+</script>
