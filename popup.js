e006e82ae78eced7a3bd97bac37ead133094f2ad
firemax13 <83276292+firemax13@users.noreply.github.com>
2021-04-29 14:20:05 +0800

    Delete popup.js

--- popup.js
+++
@@ -1,7 +0,0 @@
-<script>
-// When the user clicks on <div>, open the popup
-function myFunction() {
-  var popup = document.getElementById("mybtcpopup");
-  popup.classList.toggle("show");
-}
-</script>
